f79ebc794b4428b87e27e6ae042248a481ed4f3b
s102034013 <xu3cj84wu0h9@gmail.com>
2019-04-03 14:00:14 -0700

    email function

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,12 +3,14 @@ const multer = require('multer')
 const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+const { sendWelcomeEmail, sendCancelationEmail } = require('../emails/account')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
 	const user = new User(req.body)
 	try {
 		await user.save()
+		sendWelcomeEmail(user.email, user.name)
 		const token = await user.generateAuthToken()
 		res.status(201).send({ user, token })
 	} catch (e) {
@@ -83,8 +85,8 @@ router.delete('/users/me', auth, async (req, res) => {
 		// if (!user) {
 		// 	return res.status(404).send()
 		// }
-
 		await req.user.remove()
+		sendCancelationEmail(req.user.email, req.user.name)
 		res.send(req.user)
 	} catch (e) {
 		res.statusA(500).send()
